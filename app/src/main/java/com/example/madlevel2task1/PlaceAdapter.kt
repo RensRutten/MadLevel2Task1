6b29ed67a543d6ef127fe980c1f9f1e960dcd600
RensRutten <rens.rutten@hva.nl>
2020-09-14 13:41:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/madlevel2task1/PlaceAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.madlevel2task1
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel2task1.databinding.ItemPlaceBinding
+import kotlinx.android.synthetic.main.item_place.view.*
+
+    public class PlaceAdapter(private val places: List<Place>) :
+        RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
+
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(
+                LayoutInflater.from(parent.context).inflate(R.layout.item_place, parent, false)
+            )
+        }
+
+        override fun getItemCount(): Int {
+            return places.size
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.bind(places[position])
+        }
+
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+
+            val binding = ItemPlaceBinding.bind(itemView)
+
+
+            fun bind(place: Place) {
+                binding.ivPlace.setImageResource(place.imageResId)
+                binding.tvPlace.text = place.name
+            }
+        }
+    }
